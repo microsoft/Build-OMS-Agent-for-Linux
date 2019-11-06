286e875fa136101b16c3cade0870d1735ee32f2b
Abderrahmane Benbachir <abderb@microsoft.com>
2019-11-06 13:28:57 -0800

    adding support for signing pipeline

---
+++ pipeline-dsc-signing.cmd
@@ -0,0 +1,5 @@
+mkdir pipeline-signing
+
+cp c:\temppriordrop\current\Linux_ULINUX_1.0_x64_64_Release\dsc .pipeline-signing\
+mkdir "pipeline-signing\dsc\signing"
+cp pipeline-signing\dsc\*.sha256sums pipeline-signing\dsc\signing
