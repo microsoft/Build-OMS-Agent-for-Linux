ceaa614bd91b78a40413ca790ec5eed1ddcc0967
Abderrahmane Benbachir <abderb@microsoft.com>
2019-11-06 14:35:44 -0800

    fix cmd script

--- pipeline-dsc-signing.cmd
+++ pipeline-dsc-signing.cmd
@@ -1,5 +1,11 @@
 mkdir pipeline-signing
 
+dir c:\temppriordrop\current\Linux_ULINUX_1.0_x64_64_Release\dsc
+
 cp c:\temppriordrop\current\Linux_ULINUX_1.0_x64_64_Release\dsc .pipeline-signing\
+
 mkdir "pipeline-signing\dsc\signing"
-cp pipeline-signing\dsc\*.sha256sums pipeline-signing\dsc\signing
+
+Xcopy /E /I pipeline-signing\dsc\*.sha256sums pipeline-signing\dsc\signing
+
+dir pipeline-signing\dsc\signing
