b719df20badd5b3d5406f81614b91f3434c2fa18
Abderrahmane Benbachir <abderb@microsoft.com>
2019-11-06 17:10:34 -0800

    add more verbose

--- pipeline-dsc-signing.cmd
+++ pipeline-dsc-signing.cmd
@@ -1,22 +1,32 @@
+set DIR="%~dp0"
+cd /d %DIR%
+
+dir .
+
+dir %DIR%
+
+dir c:\temppriordrop\current\drop
+
+dir %CDP_TEMP_PRIOR_DROP_FOLDER_CONTAINER_PATH%\current\drop\Phase_1\outputs\build\buildoutput
+
+dir %CDP_TEMP_PRIOR_DROP_FOLDER_CONTAINER_PATH%\current\drop\Phase_1\outputs\build\buildoutput\Linux_ULINUX_1.0_x64_64_Release\dsc
+
+mkdir %DIR%\pipeline-signing
+
+Xcopy /E /I /Y %CDP_TEMP_PRIOR_DROP_FOLDER_CONTAINER_PATH%\current\drop\Phase_1\outputs\build\buildoutput\Linux_ULINUX_1.0_x64_64_Release\dsc %DIR%\pipeline-signing\dsc
+
+robocopy %CDP_TEMP_PRIOR_DROP_FOLDER_CONTAINER_PATH%\current\drop\linux_phase\outputs\build\drop %~dp0\%DROP% /e
+
+mkdir "%DIR%\pipeline-signing\dsc\signing"
+
+Xcopy %DIR%\pipeline-signing\dsc\*.sha256sums %DIR%\pipeline-signing\dsc\signing /Y /M
+
+dir %DIR%\pipeline-signing\dsc\signing
 
 
-dir c:\
 
-dir c:\temppriordrop\
-
-dir c:\temppriordrop\current\
-
-dir c:\temppriordrop\current\Linux_ULINUX_1.0_x64_64_Release\
-
-dir c:\temppriordrop\current\Linux_ULINUX_1.0_x64_64_Release\dsc
-
-
-mkdir .pipeline-signing
-
-Xcopy /E /I /Y c:\temppriordrop\current\Linux_ULINUX_1.0_x64_64_Release\dsc .pipeline-signing\dsc
-
-mkdir ".pipeline-signing\dsc\signing"
-
-Xcopy .pipeline-signing\dsc\*.sha256sums .pipeline-signing\dsc\signing /Y /M
-
-dir .pipeline-signing\dsc\signing
+if %ERRORLEVEL% LSS 8 (
+    exit /b 0
+) else (
+    exit /b %ERRORLEVEL%
+)
