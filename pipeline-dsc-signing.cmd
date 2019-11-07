0ec7e6e4ad48637ff07a2614afabff68fe887525
Abderrahmane Benbachir <abderb@microsoft.com>
2019-11-06 18:06:55 -0800

    fix cmd script

--- pipeline-dsc-signing.cmd
+++ pipeline-dsc-signing.cmd
@@ -1,27 +1,20 @@
-set DIR="%~dp0"
+set DIR=%~dp0
+set SIGNING_DIR=pipeline-signing
 cd /d %DIR%
 
 dir .
 
-dir %DIR%
-
-dir c:\temppriordrop\current\drop
-
-dir %CDP_TEMP_PRIOR_DROP_FOLDER_CONTAINER_PATH%\current\drop\Phase_1\outputs\build\buildoutput
-
 dir %CDP_TEMP_PRIOR_DROP_FOLDER_CONTAINER_PATH%\current\drop\Phase_1\outputs\build\buildoutput\Linux_ULINUX_1.0_x64_64_Release\dsc
 
-mkdir %DIR%\pipeline-signing
+mkdir %DIR%\%SIGNING_DIR%
 
-Xcopy /E /I /Y %CDP_TEMP_PRIOR_DROP_FOLDER_CONTAINER_PATH%\current\drop\Phase_1\outputs\build\buildoutput\Linux_ULINUX_1.0_x64_64_Release\dsc %DIR%\pipeline-signing\dsc
+Xcopy /E /I /Y %CDP_TEMP_PRIOR_DROP_FOLDER_CONTAINER_PATH%\current\drop\Phase_1\outputs\build\buildoutput\Linux_ULINUX_1.0_x64_64_Release\dsc %DIR%\%SIGNING_DIR%\dsc
 
-robocopy %CDP_TEMP_PRIOR_DROP_FOLDER_CONTAINER_PATH%\current\drop\linux_phase\outputs\build\drop %~dp0\%DROP% /e
+mkdir "%DIR%\%SIGNING_DIR%\dsc\signing"
 
-mkdir "%DIR%\pipeline-signing\dsc\signing"
+Xcopy %DIR%\%SIGNING_DIR%\dsc\*.sha256sums %DIR%\%SIGNING_DIR%\dsc\signing /Y /M
 
-Xcopy %DIR%\pipeline-signing\dsc\*.sha256sums %DIR%\pipeline-signing\dsc\signing /Y /M
-
-dir %DIR%\pipeline-signing\dsc\signing
+dir %DIR%\%SIGNING_DIR%\dsc\signing
 
 
 
--- pipeline-package-signed-modules.cmd
+++ pipeline-package-signed-modules.cmd
@@ -1,1 +1,12 @@
-dir
+set DIR=%~dp0
+cd /d %DIR%
+
+dir .
+
+
+
+if %ERRORLEVEL% LSS 8 (
+    exit /b 0
+) else (
+    exit /b %ERRORLEVEL%
+)
