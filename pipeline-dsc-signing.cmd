9172cc98bfc3b5430b47f86beef8c59264d60e4d
Abderrahmane Benbachir <abderb@microsoft.com>
2019-11-06 16:34:06 -0800

    add more verboose

--- pipeline-dsc-signing.cmd
+++ pipeline-dsc-signing.cmd
@@ -1,6 +1,17 @@
-mkdir .pipeline-signing
+
+
+dir c:\
+
+dir c:\temppriordrop\
+
+dir c:\temppriordrop\current\
+
+dir c:\temppriordrop\current\Linux_ULINUX_1.0_x64_64_Release\
 
 dir c:\temppriordrop\current\Linux_ULINUX_1.0_x64_64_Release\dsc
+
+
+mkdir .pipeline-signing
 
 Xcopy /E /I /Y c:\temppriordrop\current\Linux_ULINUX_1.0_x64_64_Release\dsc .pipeline-signing\dsc
 
