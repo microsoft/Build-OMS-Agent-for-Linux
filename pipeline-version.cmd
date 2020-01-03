cfcc9d300cc73178c3083b64f20e9450c2ac8b74
Abderrahmane Benbachir <abderb@microsoft.com>
2020-01-02 17:32:30 -0800

    update pipline script

--- pipeline-version.cmd
+++ pipeline-version.cmd
@@ -1,1 +1,11 @@
-echo ##vso[build.updatebuildnumber]1.12.25-%CDP_REVISION_NUMBER%
+set DIR=%~dp0
+
+cd /d %DIR%
+
+FOR /f %%i in ('FINDSTR "BUILDVERSION" "omsagent.version"') do SET "%%i"
+
+SET BUILD_VERSION=%OMS_BUILDVERSION_MAJOR%.%OMS_BUILDVERSION_MINOR%.%OMS_BUILDVERSION_PATCH%-%OMS_BUILDVERSION_BUILDNR%_%CDP_DEFINITION_BUILD_COUNT%
+
+echo ##vso[build.updatebuildnumber]%BUILD_VERSION%
+
+exit /b 0
