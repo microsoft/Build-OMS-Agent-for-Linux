95e7338a29465faf1829d4095e12326304c6b2e7
Abderrahmane Benbachir <abderb@microsoft.com>
2020-01-17 15:11:47 -0800

    update pipeline script to pick oms version from omsagent.version file

---
+++ pipeline-version.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source $DIR/omsagent.version
+BUILD_VERSION="${OMS_BUILDVERSION_MAJOR}.${OMS_BUILDVERSION_MINOR}.${OMS_BUILDVERSION_PATCH}-${OMS_BUILDVERSION_BUILDNR}_${CDP_DEFINITION_BUILD_COUNT}"
+
+echo "##vso[build.updatebuildnumber]$BUILD_VERSION"
+exit 0
