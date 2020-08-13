ecfc7150e2302424cbe314debba1acade21b09f4
Abderrahmane Benbachir <abderb@microsoft.com>
2020-08-13 14:02:18 -0700

    ev2 packaging

---
+++ pipeline-extension-packaging-restore.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "***************************************************************************"
+echo "***************************************************************************"
+echo "***************************************************************************"
+echo "Start of Restore "
+echo "***************************************************************************"
+echo "***************************************************************************"
+echo "***************************************************************************"
+
+SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "ls -la $SOURCE_DIR"
+ls -la $SOURCE_DIR
+
+sudo apt install -y tree
+
+echo Install Powershell package
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+sudo add-apt-repository universe
+sudo apt-get install -y powershell
+pwsh --version
+
+
+echo Cloning azure-linux-extensions ...
+git clone --recursive https://github.com/Azure/azure-linux-extensions.git $SOURCE_DIR/azure-linux-extensions
--- pipeline-oms-signing.cmd
+++ pipeline-oms-signing.cmd
@@ -3,18 +3,15 @@ set SIGNING_DIR=pipeline-signing
 cd /d %DIR%
 
 dir .
+dir %DIR%drop
 
-dir %CDP_TEMP_PRIOR_DROP_FOLDER_CONTAINER_PATH%\current\drop
+mkdir "%DIR%%SIGNING_DIR%\oms-signing"
 
-mkdir %DIR%\%SIGNING_DIR%
+Xcopy /Y %DIR%drop\Build_x64\outputs\build\buildoutput\Linux_ULINUX_1.0_x64_64_Release\*.sha256sums %DIR%\%SIGNING_DIR%\oms-signing
 
-mkdir "%DIR%\%SIGNING_DIR%\oms-signing"
+dir %DIR%%SIGNING_DIR%\oms-signing
 
-Xcopy /Y %CDP_TEMP_PRIOR_DROP_FOLDER_CONTAINER_PATH%\current\drop\Build_x64\outputs\build\buildoutput\Linux_ULINUX_1.0_x64_64_Release\*.sha256sums %DIR%\%SIGNING_DIR%\oms-signing
-
-dir %DIR%\%SIGNING_DIR%\oms-signing
-
-cd %DIR%\%SIGNING_DIR%\oms-signing
+cd %DIR%%SIGNING_DIR%\oms-signing
 
 ren *.sha256sums *.asc
 
