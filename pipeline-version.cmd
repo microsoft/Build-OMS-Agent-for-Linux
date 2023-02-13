6439b86974249cd7d8834f182392f42250ab744d
Narine Mossikyan <narinem@microsoft.com>
2023-02-13 11:22:46 -0800

    update versioning script to work in OB

--- pipeline-version.cmd
+++ pipeline-version.cmd
@@ -7,5 +7,6 @@ FOR /f %%i in ('FINDSTR "BUILDVERSION" "omsagent.version"') do SET "%%i"
 SET BUILD_VERSION=%OMS_BUILDVERSION_MAJOR%.%OMS_BUILDVERSION_MINOR%.%OMS_BUILDVERSION_PATCH%-%OMS_BUILDVERSION_BUILDNR%_%CDP_DEFINITION_BUILD_COUNT%
 
 echo ##vso[build.updatebuildnumber]%BUILD_VERSION%
+echo ##vso[task.setvariable variable=CUSTOM_VERSION;]%BUILD_VERSION%
 
 exit /b 0
