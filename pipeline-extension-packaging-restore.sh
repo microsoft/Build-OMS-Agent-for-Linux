46f0e88c231574c4e88beb534dd0d56a9cf20089
Narine Mossikyan <narinem@microsoft.com>
2023-10-12 18:01:31 -0700

    pull omsagent ext release 1.16.0 code base

--- pipeline-extension-packaging-restore.sh
+++ pipeline-extension-packaging-restore.sh
@@ -25,3 +25,6 @@ pwsh --version
 
 echo Cloning azure-linux-extensions ...
 git clone --recursive https://github.com/Azure/azure-linux-extensions.git $SOURCE_DIR/azure-linux-extensions
+cd $SOURCE_DIR/azure-linux-extensions
+git checkout OMSAgent_v1.17.0
+git submodule update --init --recursive
